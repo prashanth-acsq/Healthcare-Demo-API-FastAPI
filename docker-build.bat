c8cc56cde8c0e662121b851ba02cef65e6a977de
pchandrasekaran1595 <jackstapleton920@gmail.com>
2022-07-06 22:57:47 +0300

    Updated batch files to use environment variables

--- docker-build.bat
+++ docker-build.bat
@@ -1,1 +1,1 @@
-start /MAX cmd /c "cls && title Build Docker Image && docker build -t prashanthacsq/healthcare-demo-api:1.0 . && timeout /t 10 /nobreak"
+start /MAX cmd /c "cls && title Build Docker Image && docker build -t %DOCKER_USERNAME%/healthcare-demo-api:1.0 . && timeout /t 10 /nobreak"
--- docker-run.bat
+++ docker-run.bat
@@ -1,1 +1,1 @@
-start /MAX cmd /c "cls && title Run Docker Container && docker run -d --name hc-demo-container -p 10000:10000 prashanthacsq/healthcare-demo-api:1.0 && timeout /t 10 /nobreak"
+start /MAX cmd /c "cls && title Run Docker Container && docker run -d --name hc-demo-container -p 10000:10000 %DOCKER_USERNAME%/healthcare-demo-api:1.0 && timeout /t 10 /nobreak"
